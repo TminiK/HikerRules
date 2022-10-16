54a154ff29318528072104c5612a25417669688a
Mark <luo9757@gmail.com>
2022-10-16 10:00:43 +0800

    Update 7719.js

--- GHS/7719.js
+++ GHS/7719.js
@@ -1,14 +1,14 @@
 const baseParse = _ => {
   let d = [];
-  const list = parseDomForArray(getResCode(), '.content&&.grid_item');
+  const list = parseDomForArray(getResCode(), '.stui-vodlist&&li');
   for (let j in list) {
     d.push({
-      title: parseDomForHtml(list[j], '.description&&Text'),
-      desc: parseDomForHtml(list[j], '.num&&Text'),
-      pic_url: parseDom(list[j], 'img&&src'),
-      url: $(parseDom(list[j],'a&&href')).lazyRule(_ => {
+      title: parseDomForHtml(list[j], '.title&&Text'),
+      desc: parseDomForHtml(list[j], '.sub&&Text'),
+      pic_url: parseDom(list[j], '.stui-vodlist__thumb&&data-original'),
+      url: $(parseDom(list[j],'.stui-vodlist__thumb&&href')).lazyRule(_ => {
         const html = fetch(input)
-        const script = parseDomForHtml(html, '.bofang_box&&script&&Html')
+        const script = parseDomForHtml(html, '.stui-player__video&&script&&Html')
         eval(script)
         return player_data.url
       })
